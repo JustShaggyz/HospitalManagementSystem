d8082f0a9f52faaca978bee46868b41cefea68c6
Shaggyz <pavel.chervenyashki@gmail.com>
2022-01-08 14:25:25 +0200

    initial

---
+++ cscb869hospital/src/main/java/com/informatics/cscb869hospital/Cscb869hospitalApplication.java
@@ -0,0 +1,13 @@
+package com.informatics.cscb869hospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cscb869hospitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cscb869hospitalApplication.class, args);
+	}
+
+}
---
+++ cscb869hospital/src/test/java/com/informatics/cscb869hospital/Cscb869hospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.informatics.cscb869hospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cscb869hospitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
